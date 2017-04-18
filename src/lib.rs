0d457721e18c24b6cc33975d3503e5f770f82d84
rightfold <rightfold@gmail.com>
2017-04-18 13:14:26 +0200

    Draft

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,6 @@
 extern crate libc;
+#[macro_use]
+extern crate nom;
 extern crate num;
 extern crate typed_arena;
 
@@ -8,6 +10,7 @@ pub mod codegen;
 pub mod interpret;
 pub mod lex;
 pub mod parse;
+pub mod parse_nom;
 pub mod syntax;
 pub mod thread;
 pub mod value;
